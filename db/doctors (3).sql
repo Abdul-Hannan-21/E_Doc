b86a4cd93909ffd877447999fb66b3374bcb434e
Abdul Hannan <abdul.hannan@stud.th-deg.de>
2024-06-05 21:05:16 +0500

    error fix

--- db/doctors (3).sql
+++ db/doctors (3).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 12, 2024 at 11:14 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Host: db:3306
+-- Generation Time: Jun 05, 2024 at 04:02 PM
+-- Server version: 8.4.0
+-- PHP Version: 8.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,14 +28,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `patients` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `phone` varchar(15) NOT NULL,
   `image_name` varchar(255) NOT NULL,
   `Diagnosis` text NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `user_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `patients`
@@ -46,7 +46,10 @@ INSERT INTO `patients` (`id`, `name`, `email`, `phone`, `image_name`, `Diagnosis
 (10, 'my new edit', 'abdulhannan.rana2002@gmail.com', '1112332', 'uploaded_images/27_radiopaedia.png', 'chest pain', 2),
 (11, 'doc 1 pa', 'newtonnewton0003@gmail.com', '2323232', 'uploaded_images/31_radiopaedia.png', 'cancer', 1),
 (12, 'my pa', 'admin@gmail.com', '2323232', '', 'couging', 2),
-(14, 'Abdul ', 'newtonnewton0003@gmail.com', '1112332', 'uploaded_images/18_Electronic_medical_record_3___1_.jpg', 'cancer', 3);
+(14, 'Abdul ', 'newtonnewton0003@gmail.com', '1112332', 'uploaded_images/18_Electronic_medical_record_3___1_.jpg', 'cancer', 3),
+(15, 'alex', 'alex@gmail.com', '12123', 'uploaded_images/2_flower_729513.jpg', 'pain', 4),
+(16, 'test', 'test@gmail.com', '111', 'uploaded_images/4_alberta_2297204.jpg', 'test', 4),
+(17, 'test', 'test@gmail.com', '1234', 'uploaded_images/9_background_gf14a9789d_1920.jpg', 'pain', 4);
 
 -- --------------------------------------------------------
 
@@ -55,21 +58,21 @@ INSERT INTO `patients` (`id`, `name`, `email`, `phone`, `image_name`, `Diagnosis
 --
 
 CREATE TABLE `users` (
-  `Id` int(11) NOT NULL,
+  `Id` int NOT NULL,
   `Username` varchar(50) NOT NULL,
   `Email` varchar(50) NOT NULL,
-  `Age` int(11) NOT NULL,
   `Password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`Id`, `Username`, `Email`, `Age`, `Password`) VALUES
-(1, 'doc', 'doc@gmail.com', 0, '123'),
-(2, 'my', 'my@gmail.com', 0, '321'),
-(3, 'newdoc', 'newdoc@gmail.com', 0, '123');
+INSERT INTO `users` (`Id`, `Username`, `Email`, `Password`) VALUES
+(1, 'doc', 'doc@gmail.com', '123'),
+(2, 'my', 'my@gmail.com', '321'),
+(3, 'newdoc', 'newdoc@gmail.com', '123'),
+(4, 'john1', 'john1@gmail.com', '1234');
 
 --
 -- Indexes for dumped tables
@@ -96,13 +99,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
