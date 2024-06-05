506c3e3a18617eeb6227f4c9e62e5be2d9447d7c
Abdul Hannan <128396412+Abdul-Hannan-21@users.noreply.github.com>
2024-06-05 17:08:10 +0000

    error fix

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'd95407f6df7ef40bb6dab8e515ffcd7cdc199345',
+        'reference' => 'b86a4cd93909ffd877447999fb66b3374bcb434e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'd95407f6df7ef40bb6dab8e515ffcd7cdc199345',
+            'reference' => 'b86a4cd93909ffd877447999fb66b3374bcb434e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
